7bd274750eb1da0a5d5770c215679cf3c00bb5f4
sebastianregelmann <163838992+sebastianregelmann@users.noreply.github.com>
2025-03-17 17:12:20 +0100

    Update 01-img-manipulation.problem.py
    tutorial 01 done

--- tutorials/src/01-img-manipulation.problem.py
+++ tutorials/src/01-img-manipulation.problem.py
@@ -4,10 +4,11 @@
 # Load, resize and rotate an image. And display it to the screen.
 
 # TODO First step is to import the opencv module which is called 'cv2'
-
+import cv2
 # TODO Check the opencv version
-
-# TODO Load an image with image reading modes using 'imread'
+print(cv2.__version__)
+# TODO Load an image with image reading modes using 'imread',
+image = cv2.imread("./tutorials/data/images/logo.png", cv2.IMREAD_COLOR)
 # cv2.IMREAD_UNCHANGED  - If set, return the loaded image as is (with alpha
 #                         channel, otherwise it gets cropped). Ignore EXIF
 #                         orientation.
@@ -17,9 +18,14 @@
 #                         color image.
 
 # TODO Resize image with 'resize'
-
+height = 30
+width = 800
+image = cv2.resize(image, (height, width))
 # TODO Rotate image (but keep it rectangular) with 'rotate'
-
+image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 # TODO Save image with 'imwrite'
-
+#cv2.imwrite(filename=r"C:\Users\sebas\Desktop\GDV\GDV_SoSe2025\Test\test.jpg", img=image)
 # TODO Show the image with 'imshow'
+cv2.imshow("Logo", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
