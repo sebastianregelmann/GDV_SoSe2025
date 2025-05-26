562a78648818bf9776b612af21ff3aa7b6ddd59c
sebastianregelmann <163838992+sebastianregelmann@users.noreply.github.com>
2025-05-26 15:51:20 +0200

    14, 15 finished

--- tutorials/src/14-edge-detection.problem.py
+++ tutorials/src/14-edge-detection.problem.py
@@ -7,36 +7,58 @@ import numpy as np
 import cv2
 
 
-def show_images_side_by_side(img_A, img_B):
+def show_images_side_by_side(img_A, img_B, img_C):
     """Helper function to draw two images side by side"""
-    cv2.imshow(window_name, np.concatenate((img_A, img_B), axis=1))
+    total_image = np.concatenate((img_A, img_B,img_C), axis=1)
+    cv2.imshow(window_name, total_image)
     return
 
 
 # TODO: Define callback function
 """callback function for the sliders"""
-# Read slider positions
+def slider_change(x):
+    global window_name
+    global img_grey
+    global img
+    
+    # Read slider positions
+    blur = cv2.getTrackbarPos("Blur: ", window_name)
+    threshold_upper = cv2.getTrackbarPos("Threshold_upper: ", window_name)
+    threshold_lower = cv2.getTrackbarPos("Threshold_lower: ", window_name)
+    # Blur the image
+    blured_image = cv2.GaussianBlur(img_grey,(int(blur), int(blur)), -1)
 
-# Blur the image
-
-# Run Canny edge detection with thresholds set by sliders
-
+    # Run Canny edge detection with thresholds set by sliders
+    edges = cv2.Canny(blured_image, float(threshold_upper), float(threshold_lower))
 # Show the resulting images in one window using the show_images_side_by_side function
+    show_images_side_by_side(img,cv2.cvtColor(blured_image, cv2.COLOR_GRAY2BGR) ,cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR))
+
+    pass
+    
+
 
 # TODO Load example image as grayscale
+img = cv2.imread("./tutorials/data/images/test.jpg")
+img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Resize if needed
-
-# Clone if needed
-
-# TODO Initial Canny edge detection result creation
 
 # TODO Create window with sliders
 # Define a window name
 window_name = "Canny edge detection demo"
+
 # TODO Show the resulting images in one window
+show_images_side_by_side(img, cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR),cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR))
+
 
 # TODO Create trackbars (sliders) for the window and define one callback function
+# note that these calls lead to an assertion error as on_change is called once on creation
+cv2.createTrackbar("Blur: ", window_name, 1, 150, slider_change)
+cv2.createTrackbar("Threshold_lower: ", window_name, 30, 255, slider_change)
+cv2.createTrackbar("Threshold_upper: ", window_name, 240, 255, slider_change)
+
+
+# TODO Initial Canny edge detection result creation
+slider_change(0)
 
 # Wait until a key is pressed and end the application
 cv2.waitKey(0)
--- tutorials/src/15-feat-detection.problem.py
+++ tutorials/src/15-feat-detection.problem.py
@@ -6,33 +6,80 @@
 import numpy as np
 import cv2
 
-# TODO Define a function that detects and draws corners into the image
-# Drawing helper variables
+def generate_unique_colors(n):
+    # Generate evenly spaced hues in HSV color space
+    value_range = (0, 256)         # range for each channel [inclusive, exclusive)
 
-# Get a different color array for each of the features/corners
+    # Generate random array: shape (num_colors, 3)
+    return np.random.randint(low=value_range[0], high=value_range[1], size=(n, 3), dtype=np.uint8)
 
-# Draw a circle around each corner
 
-# Show the resulting image
+def show_images_side_by_side(img_A, img_B):
+    """Helper function to draw two images side by side"""
+    total_image = np.concatenate((img_A, img_B), axis=1)
+    cv2.imshow(window_name, total_image)
+    return
 
-# TODO Define the callback function
-# Read paremeters from slider positions
 
-# Run corner detection
+# TODO: Define callback function
+"""callback function for the sliders"""
+def slider_change(x):
+    global window_name
+    global img_grey
+    global img
+    
+    # Read slider positions
+    number = cv2.getTrackbarPos("Max_Number", window_name)
+    quality = cv2.getTrackbarPos("Quality", window_name) / 255
+    distance = cv2.getTrackbarPos("Euclidian_Distance", window_name)
+    
+    #get the features
+    features =cv2.goodFeaturesToTrack(img_grey, number, quality, distance, useHarrisDetector=True)
 
-# cv2.goodFeaturesToTrack returns corners as floating point values,
-# hence convert to integer
+    colors = generate_unique_colors(len(features))
+    
+    result_image = img.copy()
+    #draw circles 
+    for i in range(len(features)):
+        feature = features[i]
+        position = feature[0]
+        print(position)    
+        
+        selection_BGR = (int(colors[i][0]), int(colors[i][1]), int(colors[i][2]))
 
-# Call the function from above to draw the corners into the image
+        # Draw selection color circle
+        result_image = cv2.circle(img=result_image, center=(int(position[0]),int(position[1])), radius=5, color=selection_BGR, thickness=2)
+
+        
+        
+# Show the resulting images in one window using the show_images_side_by_side function
+    show_images_side_by_side(img,result_image)
+    
 
 # TODO Load example image as color image
-
-# TODO Clone image
-
+img = cv2.imread("./tutorials/data/images/window01.jpg")
+img = cv2.resize(img, (512, 512))
 # TODO Create a greyscale image for the corner detection
+img_grey = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
 
-# TODO Create a window with sliders and show resulting image
+
+# TODO Create window with sliders
+# Define a window name
+window_name = "Feature Detection"
+
+# TODO Show the resulting images in one window
+show_images_side_by_side(img, cv2.cvtColor(img_grey, cv2.COLOR_GRAY2BGR))
+
 
 # TODO Create sliders for all parameters and one callback function
+cv2.createTrackbar("Max_Number", window_name, 1, 1000, slider_change)
+cv2.createTrackbar("Quality", window_name, 1, 255, slider_change)
+cv2.createTrackbar("Euclidian_Distance", window_name, 1, 255, slider_change)
+
+
+# Call the function from above to draw the corners into the image
+slider_change(0)
 
 # Wait until a key is pressed and end the application
+cv2.waitKey(0)
+cv2.destroyAllWindows()
